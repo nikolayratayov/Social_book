d64f02e812380d857d83f25e7db79d9f9c000908
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-06-17 20:11:09 +0300

    update account settings

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()

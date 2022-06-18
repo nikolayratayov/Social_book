beb5fe297dde987169bb67cdd537c6788d44c6fd
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-06-18 17:25:08 +0300

    fixed profile picture on homepage

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_profile = Profile.objects.get(user=request.user)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 
 def signup(request):

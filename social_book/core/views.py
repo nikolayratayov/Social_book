59b3af39eb156e2899d26fcdc974fb0ef2be3e18
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-06-25 14:10:15 +0300

    User suggestions

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from itertools import chain
+import random
 
 
 @login_required(login_url='signin')
@@ -13,7 +14,6 @@ def index(request):
         user_profile = None
     else:
         user_profile = Profile.objects.get(user=request.user)
-    posts = Post.objects.all()
     user_following_list = []
     feed = []
     user_following = FollowersCount.objects.filter(follower=request.user.username)
@@ -23,7 +23,25 @@ def index(request):
         feed_lists = Post.objects.filter(user=usernames)
         feed.append(feed_lists)
     feed_list = list(chain(*feed))
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
+    # user suggestions
+    all_users = User.objects.all()
+    user_following_all = []
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+    new_suggestions_list = [x for x in list(all_users) if x not in list(user_following_all)]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if x not in list(current_user)]
+    random.shuffle(final_suggestions_list)
+    username_profile = []
+    username_profile_list = []
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    for ids in username_profile:
+        profile_list = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_list)
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 
 def signup(request):
@@ -185,4 +203,4 @@ def search(request):
             profile_list = Profile.objects.filter(id_user=ids)
             username_profile_list.append(profile_list)
         username_profile_list = list(chain(*username_profile_list))
-    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list':username_profile_list})
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
